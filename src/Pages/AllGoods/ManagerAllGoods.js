8a3fbcf9cd1a3a8d05d8d80036aa40a3a16ea15c
Mykhailenko Oleksandra <miksasha2003@gmail.com>
2023-04-29 12:22:50 +0300

    operation with goods completly work

--- src/Pages/AllGoods/ManagerAllGoods.js
+++ src/Pages/AllGoods/ManagerAllGoods.js
@@ -3,7 +3,7 @@ import ManagerLayout from "../Layout/ManagerLayout";
 import Axios from "axios";
 
 function ManagerAllGoods(props) {
-    const [id_product, setid_product] = useState('');
+    const [id_productM, setid_productM] = useState('');
     const [category_number, setcategory_number] = useState(0);
     const [product_name, setproduct_name] = useState('');
     const [producer, setproducer] = useState('');
@@ -23,9 +23,9 @@ function ManagerAllGoods(props) {
         })
     },[]);
 
-    const addGood = () => {
+    const addGood = (e) => {
+
         Axios.post("http://localhost:8888/products", {
-            id_product:id_product,
             category_number:category_number,
             product_name:product_name,
             producer:producer,
@@ -33,12 +33,11 @@ function ManagerAllGoods(props) {
         }).then(response => {
             setGoods([...goods, response.data]);
         });
-        window.location.reload();
     };
 
     const editGood = (id) => {
-        Axios.put(`http://localhost:8888/products/${id}`,{
-            id_product:id_product,
+        Axios.put(`http://localhost:8888/products`,{
+            id_product:id,
             category_number:category_number,
             product_name:product_name,
             producer:producer,
@@ -49,7 +48,7 @@ function ManagerAllGoods(props) {
     };
     const deleteGood = (id) => {
         Axios.delete(`http://localhost:8888/products/${id}`);
-        window.location.reload();
+        //window.location.reload();
     };
 
     return (
@@ -96,7 +95,7 @@ function ManagerAllGoods(props) {
                     <td><input type="checkbox" readOnly/></td>
                     <td>
                         <button onClick={() => {
-                            setid_product(g.id_product);
+                            setid_productM(g.id_product);
                             setproduct_name(g.product_name);
                             setproducer(g.producer);
                             setcharacteristics(g.characteristics);
@@ -105,7 +104,7 @@ function ManagerAllGoods(props) {
                         }} className="editButton">Редагувати</button>
                     </td>
                     <td>
-                        <button onClick={deleteGood} className="deleteButton">Видалити</button>
+                        <button onClick={()=>deleteGood(g.id_productM)} className="deleteButton">Видалити</button>
                     </td>
                 </tr>))}
             </table>
@@ -151,25 +150,25 @@ function ManagerAllGoods(props) {
                     <h2>Редагування товару</h2>
                     <form>
                         <label htmlFor="name">Назва:</label>
-                        <input type="text" id="name" name="name" required velue={product_name} onChange={(event)=>{setproduct_name(event.target.value)}}/><br/><br/>
+                        <input type="text" id="name" name="name" required value={product_name} onChange={(event)=>{setproduct_name(event.target.value)}}/><br/><br/>
                         <label htmlFor="manufacturer">Виробник:</label>
-                        <input type="text" id="manufacturer" name="manufacturer" required velue={producer} onChange={(event)=>{setproducer(event.target.value)}}/><br/><br/>
+                        <input type="text" id="manufacturer" name="manufacturer" required value={producer} onChange={(event)=>{setproducer(event.target.value)}}/><br/><br/>
                         <label htmlFor="features">Характеристики:</label>
-                        <textarea id="features" name="features" rows="4" cols="50" velue={characteristics}  onChange={(event)=>{setcharacteristics(event.target.value)}}></textarea><br/><br/>
+                        <textarea id="features" name="features" rows="4" cols="50" value={characteristics}  onChange={(event)=>{setcharacteristics(event.target.value)}}></textarea><br/><br/>
                         <label htmlFor="categories">Категорії:</label>
-                        <input type="text" id="categories" name="categories" required velue={category_number} onChange={(event)=>{setcategory_number(event.target.value)}}/><br/><br/>
+                        <input type="text" id="categories" name="categories" required value={category_number} onChange={(event)=>{setcategory_number(event.target.value)}}/><br/><br/>
                         <label htmlFor="availability">Є в наявності:</label>
-                        <input type="checkbox" id="availability" name="availability"  velue={isInStore} onChange={(event)=>{setisInStore(event.target.value)}}/><br/><br/>
+                        <input type="checkbox" id="availability" name="availability"  value={isInStore} onChange={(event)=>{setisInStore(event.target.value)}}/><br/><br/>
                         <div id="additional-fields">
                             <label htmlFor="price">Ціна товару у грн:</label>
-                            <input type="number" id="price" name="price" velue={selling_price}  onChange={(event)=>{setselling_price(event.target.value)}}/><br/><br/>
+                            <input type="number" id="price" name="price" value={selling_price}  onChange={(event)=>{setselling_price(event.target.value)}}/><br/><br/>
                             <label htmlFor="quantity">Кількість одиниць:</label>
-                            <input type="number" id="quantity" name="quantity" velue={products_number}  onChange={(event)=>{setproducts_number(event.target.value)}}/><br/><br/>
+                            <input type="number" id="quantity" name="quantity" value={products_number}  onChange={(event)=>{setproducts_number(event.target.value)}}/><br/><br/>
                             <label htmlFor="promo">Чи є товар акційним?</label>
-                            <input type="checkbox" id="promo" name="promo" value="yes" velue={promotional_product}  onChange={(event)=>{setpromotional_product(event.target.value)}}/>
+                            <input type="checkbox" id="promo" name="promo" value={promotional_product}  onChange={(event)=>{setpromotional_product(event.target.value)}}/>
                         </div>
                         <br/><br/>
-                        <button className="add_good" type="submit" onClick={()=>editGood(id_product)} name="add_good">Редагувати</button>
+                        <button className="add_good" type="submit" onClick={()=>editGood(id_productM)} name="add_good">Редагувати</button>
                     </form>
                 </div>
             </div>
